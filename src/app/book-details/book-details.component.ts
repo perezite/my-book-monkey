fc4508e8a6183025ba2c288d68ed5f1645785f49
Prez <simulo2000@gmail.com>
2022-02-17 19:45:00 +0100

    Adding book details wip

---
+++ src/app/book-details/book-details.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'bm-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.css']
+})
+export class BookDetailsComponent implements OnInit {
+  @Input() book: Book;
+  @Output() showListEvent = new EventEmitter<any>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getRating(num: number) {
+    return new Array(num);
+  }
+
+  showBookList() {
+    this.showListEvent.emit();
+  }
+}
