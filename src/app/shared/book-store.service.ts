029a1486f4c839f1c454abaf051533a4807731d0
Prez <simulo2000@gmail.com>
2022-02-21 19:06:41 +0100

    Added book search

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,7 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map, retry } from 'rxjs/operators';
 import { Book } from './book';
+import { BookFactory } from './book-factory';
+import { BookRaw } from './book-raw';
 
 @Injectable({
   providedIn: 'root'
@@ -9,19 +12,40 @@ import { Book } from './book';
 export class BookStoreService {
   private api = 'https://api4.angular-buch.com';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) { }
 
+  getAllSearch(searchTerm: string): Observable<Book[]> {
+    return this.http.get<BookRaw[]>(`${this.api}/books/search/${searchTerm}`)
+      .pipe(
+        retry(3),
+        map(booksRaw => booksRaw.map(bookRaw => BookFactory.fromRaw(bookRaw))),
+        catchError(this.errorHandler)
+      );
   }
 
   getAll(): Observable<Book[]> {
-    return this.http.get<any[]>(`${this.api}/books`);
+    return this.http.get<BookRaw[]>(`${this.api}/books`)
+      .pipe(
+        map(booksRaw => booksRaw.map(bookRaw => BookFactory.fromRaw(bookRaw))),
+        catchError(this.errorHandler)
+      );
   }
 
   getSingle(isbn: string): Observable<Book> {
-    return this.http.get<any>(`${this.api}/book/${isbn}`);
+    return this.http.get<BookRaw>(`${this.api}/book/${isbn}`)
+      .pipe(
+        retry(3),
+        map(bookRaw => BookFactory.fromRaw(bookRaw)),
+        catchError(this.errorHandler)
+      );
   }
 
   remove(isbn: string): Observable<any> {
     return this.http.delete(`${this.api}/book/${isbn}`, { responseType: 'text' });
+  }
+
+  private errorHandler(error: HttpErrorResponse): Observable<any> {
+    console.error('Fehler aufgetreten!');
+    return throwError(error);
   }
 }
